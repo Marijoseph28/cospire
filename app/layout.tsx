9db231a640cbcf88b401cb7e331f2e94d8c25974
mari_joseph <veena.j@exalogic.co>
2025-03-13 12:40:41 +0530

    Save local changes before pull

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,34 @@
 import type { ReactNode } from "react"
 import "./globals.css"
+// import localFont from 'next/font/local'
+import { Poppins } from 'next/font/google'
 
+// const gilSans = localFont({
+//   src: [
+//     {
+//       path: 'font/GIL-sans.woff2',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//   ]
+// });
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 export default function RootLayout({
   children,
 }: {
   children: ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body className="antialiased">{children}</body>
     </html>
   )
 }
 
 
-
-import './globals.css'
 
 export const metadata = {
       generator: 'v0.dev'
